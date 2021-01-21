9b824919dd73a6ce0f4658c3184e47f57ba10940
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-01-21 21:18:52 +0100

    feat: testing treesitter queries

--- lua/commented/init.lua
+++ lua/commented/init.lua
@@ -1,5 +1,5 @@
 local commented = {}
-
+local parsers = require("nvim-treesitter.parsers")
 
 function commented.test()
     print('test')
@@ -7,6 +7,25 @@ end
 
 function commented.echo_missing()
     print('echo_missing')
+    local result = commented.parse('(function_item)')
+    print(result)
+end
+
+
+function commented.parse(query)
+    print(
+        vim.inspect(
+            vim.treesitter.parse_query('rust', query)
+        )
+    )
+    local lang = vim.api.nvim_buf_get_option(0, 'ft')
+    local success, parsed_query = pcall(function()
+        return vim.treesitter.parse_query(lang, query)
+    end)
+    if not success then
+        print('sad')
+        return
+    end
 end
 
 return commented
